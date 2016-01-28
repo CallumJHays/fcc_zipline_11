fd66912e76fd50c7bd38ee52da6878af2b9078ca
callumjhays <callumjhays@gmail.com>
2016-01-28 03:46:57 +0000

    first working version

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
-'use strict';
-
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+app.get('/:time', function(req, res){
+	var time;
+	if(isNaN(Number(req.params.time)))
+		time = new Date(req.params.time);
+	else
+		time = new Date(Number(req.params.time));
+	if(!isNaN(time.getTime())){
+		res.send({ 
+			unix: time.getTime(), 
+			natural: time.toLocaleDateString('en-US', {month:'long', day:'numeric', year:'numeric'}) 
+		});
+	}
+	else {
+		res.send({ 
+			unix: null,
+			natural: null 
+		});
+	}
 });
+
+app.listen(8080);
